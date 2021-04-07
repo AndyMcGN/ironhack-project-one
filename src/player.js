87b5b95543c376dff1d593deb7319d0c95f23439
Andy <andrew.mcginn@gmail.com>
2021-04-07 20:51:28 +0200

    lives and score drawn nicely

--- src/player.js
+++ src/player.js
@@ -7,16 +7,30 @@ class Player {
     }
 
     setup() {
-        let scoreElem = createDiv("Score: <span id='score'>0</span>");
-        scoreElem.position(150,50);
-        scoreElem.class("player-info");
+        // let scoreElem = createDiv("Score: <span id='score'>0</span>");
+        // scoreElem.position(150,50);
+        // scoreElem.class("player-info");
 
-        let livesElem = createDiv("Lives: <span id='lives'>3</span>");
-        livesElem.position(150, 100);
-        livesElem.class("player-info");
+        // let livesElem = createDiv("Lives: <span id='lives'>3</span>");
+        // livesElem.position(150, 100);
+        // livesElem.class("player-info");
     }
 
+    drawLives() {
+        let xPos = 50;
 
+        text("Lives:",130, 146)
+        for (let i = 0; i < this.lives; i++) {
+            image(game.welcomeBalloons[0], xPos, 160, 45,70);
+            xPos += 60;
+        }
+    }
+
+    drawScore() {
+        image(game.scoreBalloon, 90, 20, 90,90);
+        text(this.score, 135, 60)
+
+    }
 
 
     updateScore() {
@@ -31,7 +45,7 @@ class Player {
 
     loseLife() {
         this.lives--;
-        document.querySelector("#lives").innerText = this.lives;
+        // document.querySelector("#lives").innerText = this.lives;
         if (this.lives === 0) {
             // mode = 'game-over';
             // game.gameOver();
