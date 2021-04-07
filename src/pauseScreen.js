ac5204257884c01614ae5ba6a185e26a91e63549
Andy <andrew.mcginn@gmail.com>
2021-04-07 21:16:34 +0200

    fixed bug with spacing on pause screen

--- src/pauseScreen.js
+++ src/pauseScreen.js
@@ -24,7 +24,7 @@ class PauseScreen {
 
     isCorrectPauseInput() {
         console.log(this.input.value());
-        if (this.input.value() === this.missedWord.lang2) {
+        if (this.input.value().trim() === this.missedWord.lang2) {
             this.input.hide();
             game.input.input.show();
             return true;
